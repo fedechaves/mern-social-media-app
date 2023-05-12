0d8c4a44da148637165fc402bdb8510ee29fbd44
fedechaves <federicochavesf@gmail.com>
2023-05-12 01:40:39 -0400

    match cloudinary prerequisits

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -3,7 +3,9 @@ import User from "../models/User.js";
 
 /* CREATE */
 export const createPost = async (req, res) => {
+  console.log(req.body)
   try {
+
     const { userId, description, picturePath } = req.body;
     const user = await User.findById(userId);
     const newPost = new Post({
--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
-
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -42,8 +44,8 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
-app.post("/auth/register", upload.single("picture"), register);
-app.post("/posts", verifyToken, upload.single("picture"), createPost);
+app.post("/auth/register", upload.single("file"), register);
+app.post("/posts", verifyToken, upload.single("file"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
