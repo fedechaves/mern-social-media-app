9cfcc94bde5cc45bc217c850555e55b572a472b7
fedechaves <federicochavesf@gmail.com>
2023-05-19 14:55:52 -0400

    cloudinary update

--- server/index.js
+++ server/index.js
@@ -33,7 +33,14 @@ app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
-const storage = multer.diskStorage({});
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
 const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
