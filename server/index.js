9b2de69b5e2fa901407e47e4422aa9319fb5865b
fedechaves <federicochavesf@gmail.com>
2023-06-15 12:57:19 -0400

    cors

--- server/index.js
+++ server/index.js
@@ -31,12 +31,18 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 //CORS
-const corsOptions ={
-  origin:'https://landscape-social.vercel.app', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//   origin:'https://landscape-social.vercel.app', 
+//   credentials:true,            //access-control-allow-credentials:true
+//   optionSuccessStatus:200,
+// }
+// app.use(cors(corsOptions));
+
+// CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://landscape-social.vercel.app');
+  next();
+});
 
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
