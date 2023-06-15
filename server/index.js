a657bf8f5dccd7443f98ec1f7c18fba8ead68c02
fedechaves <federicochavesf@gmail.com>
2023-06-15 11:11:33 -0400

    cors policy

--- server/index.js
+++ server/index.js
@@ -29,7 +29,13 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+
+//CORS
+app.use(cors({
+  origin: "https://landscape-social.vercel.app/",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+}));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
